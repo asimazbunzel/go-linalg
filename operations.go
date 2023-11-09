31f8f4ded51b7e5e56fd843bca2f929f3d9f0494
asb <asimazbunzel@gmail.com>
2023-11-08 23:10:33 -0300

    add method to divide two matrices, element by element

--- operations.go
+++ operations.go
@@ -121,6 +121,27 @@ func MulElem(a, b *Dense) (*Dense, error) {
 	return NewDense(aShape[0], aShape[1], res), nil
 }
 
+// DivlElem
+func DivElem(a, b *Dense) (*Dense, error) {
+	// get matrix shapes (m,n) -- rows,cols
+	aShape := a.Shape()
+	bShape := b.Shape()
+
+	// check that cols & rows in A == cols & rows in B
+	if aShape[0] != bShape[0] || aShape[1] != bShape[1] {
+		return nil, ErrMulMismatchDimensions
+	}
+
+	res := make([]float64, aShape[0]*aShape[1])
+	for i := 0; i < aShape[0]; i++ {
+		for j := 0; j < aShape[1]; j++ {
+			res[i*aShape[1]+j] = a.Data[i*aShape[1]+j] / b.Data[i*bShape[1]+j]
+		}
+	}
+
+	return NewDense(aShape[0], aShape[1], res), nil
+}
+
 // Apply
 func Apply(m *Dense, fn func(v float64) float64) *Dense {
 	// get matrix shape
